42a067939fa0eb3cc65e2743bf53da713da88212
calyha <cassandrahall2017@u.northwestern.edu>
2014-11-05 15:08:43 -0600

    C++ files
    Incomplete, first version.

---
+++ project.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+string direct("direct.txt");
+string indirect("indirect.txt");
+
+int main(){
+	ifstream fin(direct);
+	file.open(direct);
+
+	if(fin.is_open()){
+		cout << "Opened " << direct << "successfully!"\n;
+		while(!fin.eof() && a < array_size && b < array_size){
+			fin.get(array[a,b]);
+			if(a = array_size - 1){
+				b++;
+				a = 0;
+			}
+			else{
+				a++;
+			}
+		}
+
+	}
+
+	while(!file.eof()){
+		string line;
+		
+	int n;
+	int q;
+
+	if (n % q != 0){
+		throw Exception("q does not divide n");
+	}
+
+	double direct[n,n] = {2};
+	double indirect[n,n] = {1};
+return 1;
+}
